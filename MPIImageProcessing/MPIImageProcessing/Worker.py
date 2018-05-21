4738fdb8d54753322dcdd7f0d17b4fdab4217507
gradzka <monika.gradzka@tlen.pl>
2018-05-21 22:37:05 +0200

    Worker - poprawka

--- MPIImageProcessing/MPIImageProcessing/Worker.py
+++ MPIImageProcessing/MPIImageProcessing/Worker.py
@@ -2,26 +2,60 @@ from mpi4py import MPI
 import ImageProcessing as ImageProc
 from PIL import Image
 import sys
+import json
 
 
-if len(sys.argv)>3:
-    comm = MPI.COMM_SELF.Spawn(sys.executable, args=['MPIImageProcessing.py'], maxprocs=int(sys.argv[3]))
-    #pathToFile SHA
-    path = sys.argv[1]
-    action = sys.argv[2]
-    if len(sys.argv)>4:
-        option = int(sys.argv[4])
-    #read InPicture
-    INPicture = INPicture = Image.open(sys.argv[1])
-    comm.bcast(action, root=MPI.ROOT)
-    comm.bcast(INPicture, root=MPI.ROOT)
-    comm.bcast(option, root=MPI.ROOT)
-    OUTPicture = None
-    if action == "histogram":
-        my_op = MPI.Op.Create(ImageProc.my_histogram_sum)
-        histogram = comm.reduce(histogram, op=my_op, root=0)
-    else: 
-        my_op = MPI.Op.Create(ImageProc.my_sum)
-        OUTPicture=comm.reduce(OUTPicture, op=my_op, root=0)
-    OUTPicture.save("OUT_"+sys.argv[1])
-    comm.Disconnect()
+def Work():
+    try:
+        if len(sys.argv)>3:
+            maxProcNo = sys.argv[3]
+            if not maxProcNo.isdigit():
+                return
+            maxProcNo = int(maxProcNo)
+            #pathToFile SHA
+            path = sys.argv[1]
+            action = sys.argv[2]
+            if action in ["histogram", "rotation", "mirrorReflection", "RGBSelection", "negative", "shadesOfGrey", "brightness", "contrast", "gamma"]:
+                if action in ["rotation", "mirrorReflection", "RGBSelection", "brightness", "contrast", "gamma"]:
+                    if len(sys.argv)>4:
+                        if action in ["contrast", "gamma"]:
+                            option = float(sys.argv[4])
+                            if option<0.1 or option>10:
+                                return
+                        elif action in ["RGBSelection","rotation"]:
+                            option = int(sys.argv[4])
+                            if option not in [0,1,2]:
+                                return
+                        elif action == "brightness":
+                            option = int(sys.argv[4])
+                            if option <-255 or option>255:
+                                return
+                        elif action == "mirrorReflection":
+                            option = int(sys.argv[4])
+                            if option not in [0,1]:
+                                return
+                    else:
+                        return
+                comm = MPI.COMM_SELF.Spawn(sys.executable, args=['MPIImageProcessing.py'], maxprocs=maxProcNo)
+                #read InPicture
+                INPicture = Image.open(path)
+                comm.bcast(action, root=MPI.ROOT)
+                comm.bcast(INPicture, root=MPI.ROOT)
+                if len(sys.argv)>4:
+                    comm.bcast(option, root=MPI.ROOT)
+                #histogram or another
+                if action == "histogram":
+                    my_op = MPI.Op.Create(ImageProc.my_histogram_sum)
+                    histogram = comm.reduce(None, op=my_op, root=MPI.ROOT)
+                    serializedHistogram = json.dumps(histogram.__dict__)
+                else: 
+                    OUTPicture = None
+                    my_op = MPI.Op.Create(ImageProc.my_sum)
+                    OUTPicture=comm.reduce(None, op=my_op, root=MPI.ROOT)
+                    OUTPicture.save("OUT_"+sys.argv[1])
+                comm.Disconnect()
+    except Exception as e:
+        print("error: {0}".format(e))
+
+if __name__ == '__main__':
+    Work()
